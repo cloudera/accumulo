d9fd864521451dadb594c1851038bb8817e57fcb
Mike Drob <mdrob@cloudera.com>
2014-02-03 11:29:57 -0500

    CLOUDERA-BUILD. Update for cdh integration.
    * Set cdh-root as parent pom.
     - Increment failsafe-plugin version
     - Increment assembly-plugin version
     - Exclude mockito from hadoop-common
     -- Mockito brings in a different version of Hamcrest than Junit
     - Explicitly define rat-plugin version
     - specify maven-plugin-plugin 3.2
     - set other plugin versions that are set by the apache root pom upstream
     - add properties relied on that are set by apache root pom upstream
     - remove dep management for things handled by cdh-root
    * Use hadoop-tools instead of -distcp for MRv1
    * Add additional profile for CDH4 YARN
    * Disable git hash inclusion so build will work outside of the git branch the project lives in.
    * Exempting temporary build/packaging files from RAT licensing checks
    * add assembly for tarball tailored for CDH integration.
    * include example configuration files for CDH4 parcels and packages
    
    Author: Mike Drob <madrob@cloudera.com>
    Author: Bill Havanki <bhavanki@clouderagovt.com>
    Author: Sean Mackrory <sean@cloudera.com>
    Author: Sean Busbey <busbey@cloudera.com>

---
+++ conf/examples/CDH4_packages/accumulo-env.sh
@@ -0,0 +1,67 @@
+#! /usr/bin/env bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+###
+### Configure these environment variables to point to your local installations.
+###
+### The functional tests require conditional values, so keep this style:
+###
+### test -z "$JAVA_HOME" && export JAVA_HOME=/usr/local/lib/jdk-1.6.0
+###
+###
+### Note that the -Xmx -Xms settings below require substantial free memory:
+### you may want to use smaller values, especially when running everything
+### on a single machine.
+###
+
+# Make sure this points to your java install
+test -z "$JAVA_HOME"             && export JAVA_HOME=/usr/java/jdk1.6.0_31
+
+if [ -z "$HADOOP_HOME" ]
+then
+    test -z "$HADOOP_PREFIX" && export HADOOP_PREFIX=/usr/lib/hadoop
+    export HADOOP_HOME=$HADOOP_PREFIX
+else
+    HADOOP_PREFIX="$HADOOP_HOME"
+fi
+
+test -z "$HADOOP_CLIENT_HOME"    && export HADOOP_CLIENT_HOME=/usr/lib/hadoop/client-0.20
+test -z "$HADOOP_CONF_DIR"       && export HADOOP_CONF_DIR="$HADOOP_PREFIX/etc/hadoop"
+
+test -z "$ZOOKEEPER_HOME"        && export ZOOKEEPER_HOME=/usr/lib/zookeeper
+test -z "$ACCUMULO_LOG_DIR"      && export ACCUMULO_LOG_DIR=$ACCUMULO_HOME/logs
+if [ -f ${ACCUMULO_CONF_DIR}/accumulo.policy ]
+then
+   POLICY="-Djava.security.manager -Djava.security.policy=${ACCUMULO_CONF_DIR}/accumulo.policy"
+fi
+test -z "$ACCUMULO_TSERVER_OPTS" && export ACCUMULO_TSERVER_OPTS="${POLICY} -Xmx1g -Xms1g -XX:NewSize=500m -XX:MaxNewSize=500m "
+test -z "$ACCUMULO_MASTER_OPTS"  && export ACCUMULO_MASTER_OPTS="${POLICY} -Xmx2g -Xms1g"
+test -z "$ACCUMULO_MONITOR_OPTS" && export ACCUMULO_MONITOR_OPTS="${POLICY} -Xmx2g -Xms256m"
+test -z "$ACCUMULO_GC_OPTS"      && export ACCUMULO_GC_OPTS="-Xmx256m -Xms256m"
+test -z "$ACCUMULO_GENERAL_OPTS" && export ACCUMULO_GENERAL_OPTS="-XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -Djava.net.preferIPv4Stack=true"
+test -z "$ACCUMULO_OTHER_OPTS"   && export ACCUMULO_OTHER_OPTS="-Xmx1g -Xms256m"
+# what do when the JVM runs out of heap memory
+export ACCUMULO_KILL_CMD='kill -9 %p'
+
+### Optionally look for hadoop and accumulo native libraries for your
+### platform in additional directories. (Use DYLD_LIBRARY_PATH on Mac OS X.)
+### May not be necessary for Hadoop 2.x or using an RPM that installs to
+### the correct system library directory.
+# export LD_LIBRARY_PATH=${HADOOP_PREFIX}/lib/native/${PLATFORM}:${LD_LIBRARY_PATH}
+
+# Should the monitor bind to all network interfaces -- default: false
+# export ACCUMULO_MONITOR_BIND_ALL="true"
