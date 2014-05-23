d0b5746c0033fcbfed4546d33b449188828db694
Josh Elser <elserj@apache.org>
2014-05-23 14:40:37 -0400

    ACCUMULO-2842 Allow configuration of FSDataOutputStream method used to sync WALs.
    Reason: Performance
    Author: Josh Elser <elserj@apache.org>
    Ref: CDH-19749
    
    A critical piece of the bigtable design is using write-ahead logs to ensure
    recovery after node failure. The append only file structure is meant to provide
    a durable log of events so that the correct state can be reconstructed after
    a failure when key-values have not been yet serialized to disk.
    
    Hadoop 2 offers two levels of durability in regards to writes to files in HDFS. HFlush
    ensures that all others clients who try to read the file you just wrote data to will
    see that new data that was written. HSync will ensure that the data you wrote for that
    file is actually sent to the underlying disk. As such, HFlush does not provide durability
    when the node crashes without that data being flushed to the drive, while HSync does. HSync
    does, however, increase the amount of time needed for sync/flush to complete (as hitting disk
    consistently is slower than not).
    
    (cherry picked from commit f654e7faadb9e94e2923087af334a707a4d2c81a)
    
    Conflicts:
    	core/src/main/java/org/apache/accumulo/core/conf/Property.java
    	server/tserver/src/main/java/org/apache/accumulo/tserver/log/DfsLogger.java

--- server/tserver/src/main/java/org/apache/accumulo/tserver/log/DfsLogger.java
+++ server/tserver/src/main/java/org/apache/accumulo/tserver/log/DfsLogger.java
@@ -336,21 +336,17 @@ public class DfsLogger {
       else
         logFile = fs.create(new Path(logPath), true, 0, replication, blockSize);
 
+      String syncMethod = conf.getConfiguration().get(Property.TSERV_WAL_SYNC_METHOD);
       try {
-        NoSuchMethodException e = null;
-        try {
-          // sync: send data to datanodes
-          sync = logFile.getClass().getMethod("sync");
-        } catch (NoSuchMethodException ex) {
-          e = ex;
-        }
         try {
           // hsync: send data to datanodes and sync the data to disk
-          sync = logFile.getClass().getMethod("hsync");
-          e = null;
-        } catch (NoSuchMethodException ex) {}
-        if (e != null)
-          throw new RuntimeException(e);
+          sync = logFile.getClass().getMethod(syncMethod);
+        } catch (NoSuchMethodException ex) {
+          log.warn("Could not find configured " + syncMethod + " method, trying to fall back to old Hadoop sync method", ex);
+
+          // sync: send data to datanodes
+          sync = logFile.getClass().getMethod("sync");
+        }
       } catch (Exception e) {
         throw new RuntimeException(e);
       }
