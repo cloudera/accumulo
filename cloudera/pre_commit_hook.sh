bb1756b157058d8cd8db6e03e7c2b608f99be913
Sean Busbey <busbey@cloudera.com>
2018-08-15 11:04:06 -0500

    CLOUDERA-BUILD have precommit run go through the unit tests.
    Change-Id: I4e3bb1e0b5ab1af2d74e5acb915e56f8c7aaf9f3
    Ref: CDH-71597

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -3,5 +3,15 @@
 set -ex
 set -o pipefail
 
-echo "NO-OP Build"
-exit 0
+JAVA_HOME="${JAVA_HOME:-${JAVA_1_8_HOME}}"
+export JAVA_HOME
+PATH="${MAVEN_HOME:-${MAVEN_3_5_0_HOME}}/bin:${JAVA_HOME}/bin:${PATH}"
+export PATH
+
+MVN_REPO_LOCAL=$HOME/.m2/repository${M2_REPO_SUFFIX}
+declare -a MVN_ARGS=(--batch-mode --fail-at-end -Dmaven.repo.local="${MVN_REPO_LOCAL}")
+mvn -version
+# install the tar.gz for native stuff locally, because the test module needs it at compile scope.
+# install the docs manual, because the top level module needs it at compile scope.
+mvn "${MVN_ARGS[@]}" -DskipTests -am -pl server/native -pl docs install
+mvn "${MVN_ARGS[@]}" test
